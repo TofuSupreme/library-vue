297931eaceda51c5108bfa41272e2b90834b70ec
tofusupreme <yunishbacon@gmail.com>
2022-08-11 20:04:59 +0900

    installed UUID depdency

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,21 @@
+<!-- Using Vue-uuid for unique IDs-->
 <template>
   <div class="">
   <h1> Welcome to your personal library</h1>
   <div>
   <div class="create-new-book">
   <label> Title: </label>
-    <input type="text" v-model="newBook" @keydown.enter="addBook" placeholder="Add a title">
-    <button @click="addBook">Add</button>
+    <input type="text" v-model="newBookInput" @keydown.enter="addBook" placeholder="Add a title">
+    <button @click="addBook">Add Book</button>
   </div>
-    <!-- <form @submit="onSubmit" class="add-form">
+  <div class="book" v-for="book in books" :key="book.id">
+    <span>{{book.name}}</span>
+    <!-- <span> {{book.author}}</span>
+    <span> {{book.complete}}</span> -->
+    <span class="delete-book" @click="deleteBook(book.id)">X</span>
+  </div>
+    <!-- <form @submit="onSu
+    bmit" class="add-form">
       <label> Book Name</label>
       <input type="text" v-model="addNewBook" @keydown.enter="addBook" name="book" placeholder="Enter a book title"/>
       <label> Author</label>
@@ -22,7 +30,29 @@
 </template>
 
 <script>
+import {uuid} from 'vue-uuid';
+
 export default {
-  name: 'IndexPage'
+  name: 'App',
+  data() {
+    return {
+      books: [],
+      newBookInput: '',
+    }
+  },
+  methods: {
+    addBook() {
+      let newBook = {
+        id: uuid.v4(),
+        name: this.newBookInput
+      }
+      this.books.push(newBook)
+console.log(newBook)
+    },
+
+    deleteBook(bookId) {
+
+    }
+  }
 }
 </script>
