da4a7075ec656083f568f4aee5cd2a5bfc1da22c
tofusupreme <yunishbacon@gmail.com>
2022-08-15 10:42:17 +0900

    made more style changes to index

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="scroll-smooth">
+  <main class="scroll-smooth flex-column">
     <h1> Welcome to your personal library</h1>
     <FormulateForm v-model="bookValues" :key="bookFormKey" type="form" @submit="addBook">
       <FormulateInput v-model="bookValues.bookTitle" type="text" name="bookTitle" label="Title" placeholder="Book title"
@@ -22,16 +22,18 @@
       <FormulateInput type="submit" label="Submit" outer-class=" my-2 border p-2 hover:bg-green-500 cursor-pointer" />
     </FormulateForm>
 
-    <div v-for="book in books" :key="book.id">
-      <ul>
+    <div class="bg-gray-400 border-white py-2 px-2">
+      <p> List of your books </p>
+      <ul v-for="book in books" :key="book.id">
         <li class="py-3 px-3 mb-1 bg-white text-black rounded-lg shadow-lg space-y-2 flex-row hover:bg-green-200">
           {{book.bookTitle}} by {{book.bookAuthor}} - {{book.bookDate}}
           <button
-            class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-black hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
+            class="px-4 py-1 justify-end text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-black hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
             @click="removeBook(book.id)">X</button>
         </li>
       </ul>
     </div>
+    <footer class="py-4"> &copy; TofuSupreme 2022 </footer>
   </main>
 </template>
 
